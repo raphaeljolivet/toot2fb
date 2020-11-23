ec44308c2dd2d4a6d581388744b670b3c9cb584d
Raphael Jolivet <raphael.jolivet@mines-paristech.fr>
2020-11-23 18:13:07 +0100

    integrate Flask server

--- main.py
+++ main.py
@@ -2,7 +2,11 @@
 import feedparser
 from html2text import HTML2Text
 from bs4 import BeautifulSoup
-import facebook
+from rfeed import *
+from datetime import datetime
+from time import mktime
+from flask import Flask
+from flask import request, Response
 
 URL="https://mamot.fr/@testrjo.rss"
 APP_ID="370919417317291"
@@ -19,45 +23,70 @@ TYPE_IMAGE = "image"
 html2text = HTML2Text()
 html2text.ignore_links = True
 
-for entry in feed['entries'] :
+def transform_rss(filter=None) :
+    items= []
+    for entry in feed['entries'] :
 
-    type = TYPE_TEXT
+        type = TYPE_TEXT
 
-    summary = entry['summary_detail']
-    entry_link = entry["link"]
-    out_entry_link = entry_link
+        summary = entry['summary_detail']
+        entry_link = entry["link"]
+        out_entry_link = entry_link
 
-    if 'links' in entry :
-        for link in entry['links'] :
-            if 'image' in link.type :
-                type = TYPE_IMAGE
-                out_entry_link = link.href
-                break
+        if 'links' in entry :
+            for link in entry['links'] :
+                if 'image' in link.type :
+                    type = TYPE_IMAGE
+                    out_entry_link = link.href
+                    break
 
-    if summary.type == 'text/html' :
+        if summary.type == 'text/html' :
 
-        html = BeautifulSoup(summary.value, 'html.parser')
-        summary_txt = html2text.handle(summary.value)
+            html = BeautifulSoup(summary.value, 'html.parser')
+            summary_txt = html2text.handle(summary.value)
 
-        for link in html.findAll('a'):
-            if not (link.get("class") and "mention" in link.get("class")) :
-                type = TYPE_LINK
-                out_entry_link = link.get('href')
-                break
+            for link in html.findAll('a'):
+                if not (link.get("class") and "mention" in link.get("class")) :
+                    type = TYPE_LINK
+                    out_entry_link = link.get('href')
+                    break
 
-    else :
-        summary_txt = summary.value
+        else :
+            summary_txt = summary.value
 
 
 
-    summary_txt += "\n[transféré depuis %s]" % entry_link
-    print("type", type)
-    print("out_link", out_entry_link)
-    print("text", summary_txt)
-    print("\n\n")
+        summary_txt += "\n[transféré depuis %s]" % entry_link
+        summary_html = "<br/>".join(summary_txt.splitlines())
 
-"""
-token = facebook.GraphAPI().get_app_access_token(APP_ID, APP_SECRET)
-graph = facebook.GraphAPI(token)
-page = graph.get_object(PAGE_ID)
-print(page)"""
+        if filter is not None and type != filter :
+            continue
+
+        items.append(Item(
+            title=entry.title,
+            link=out_entry_link,
+            description=summary_html,
+            guid=Guid(entry.guid),
+            pubDate=datetime.fromtimestamp(mktime(entry.published_parsed))
+        ))
+
+
+    outfeed = Feed(
+        title = feed.feed.title,
+        link = feed.feed.link,
+        description = feed.feed.description,
+        items = items)
+    return outfeed
+
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def entry_point():
+    res = transform_rss(request.args.get("filter"))
+    return Response(res.rss(), mimetype='text/xml')
+
+if __name__ == '__main__':
+    app.run(debug=True)
